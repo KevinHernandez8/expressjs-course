9f44bc4bc81c3d36d2fdb4c183b676fd827e5d88
Kevin <kevin.hernandezr8@gmail.com>
2019-10-27 11:27:50 -0500

    Manejo del template engine pug

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+const users = [
+  {
+    name: 'Alejandro',
+    age: 21
+  },
+  {
+    name: 'Daniel',
+    age: 15
+  }
+]
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  // res.send('respond with a resource');
+  res.render('users', {users: users})
 });
 
 module.exports = router;
